9efaa0933f93b102434ad75b94da13683143922c
leonanda1013 <leonandaprabowo@gamil.com>
2023-11-13 15:00:33 +0700

    update

--- tambah.java
+++ tambah.java
@@ -1,5 +1,5 @@
  import java.util.Scanner;
-public class Booking {
+public class tambah {
    
 
 
---
+++ tipedata.java
@@ -0,0 +1,9 @@
+public class tipedata {
+
+    public static void main(String[] args) {
+        int angka = 11;
+        System.out.println("kelas berapa sekarang: " + angka);
+        String[] namaHari =  {"Senin", "Selasa", "Rabu"};
+        System.out.println(namaHari); 
+    }
+}
